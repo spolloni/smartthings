8bc49e790e2cf13d09d3d214d81d76bf33f4f6be
Unknown <stefano_polloni@brown.edu>
2019-08-22 22:50:26 -0400

    talk2hifi: test zone enable

--- testing/talktohifi.js
+++ testing/talktohifi.js
@@ -1,1 +1,7 @@
 console.log('Hello World');
+
+const net = require('net');
+  const client = new net.Socket();
+  client.connect(23, 192.168.2.24, function() {
+    client.write("*Z4ENABLE1");
+  });
